22cea7f073c2ef8e4f3aedbee662d49d9a2a543c
Vasyl Stepanov <58212703+vasiastep@users.noreply.github.com>
2020-06-01 17:38:14 +0300

    Final

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,57 +3,60 @@ import { ModalContext } from '../context/Modal/ModalContext'
 import { ThemeContext } from '../context/Theme/ThemeContext'
 
 export const Modal = ({ options, newsItem }) => {
+	const { hideModal, isOpened } = useContext(ModalContext)
+	const { darkTheme } = useContext(ThemeContext)
 
-    const { hideModal, isOpened } = useContext(ModalContext)
-    const { darkTheme } = useContext(ThemeContext)
-    
-    const itemContent = (
-    <>
-        <div className="modal-news-img-and-title">
-            <h2 className="modal-news-title">{newsItem.title || ''}</h2>
-            { newsItem.urlToImage ? <img className="modal-news-img" src={newsItem.urlToImage} alt="Oops..." /> : null }
-        </div>
-        { newsItem.description ?         
-            <div className="modal-news-main-body">
-                <p className="modal-news-description">{newsItem.description || ''}</p>
-                <div className="modal-news-link">See more on&nbsp;
-                    <a 
-                        className="modal-news-link-target" 
-                        href={newsItem.url} 
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >{newsItem.source.name}</a>
-                </div>
-            </div> : 
-        null }
-    </>)
-    
-    const closeModal = () => hideModal()
+	const itemContent = (
+		<>
+			<div className="modal-news-img-and-title">
+				<h2 className="modal-news-title">{newsItem.title || ''}</h2>
+				{newsItem.urlToImage ? (
+					<img className="modal-news-img" src={newsItem.urlToImage} alt="Oops..." />
+				) : null}
+			</div>
+			{newsItem.description ? (
+				<div className="modal-news-main-body">
+					<p className="modal-news-description">{newsItem.description || ''}</p>
+					<div className="modal-news-link">
+						See more on&nbsp;
+						<a
+							className="modal-news-link-target"
+							href={newsItem.url}
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							{newsItem.source.name}
+						</a>
+					</div>
+				</div>
+			) : null}
+		</>
+	)
 
-    return (
-        <div className={`vmodal ${isOpened ? 'open' : 'hide'}`}>
-            <div className="modal-overlay">
-                <div 
-                    className={`modal-window ${darkTheme ? 'bg-dark text-white' : ''}`} 
-                    style={{top: options.positionTop}}
-                >
-                    <div className="modal-window-header">
-                        <h4 className="m-0">{options.title}</h4>
-                        {options.closable ? 
-                            <p 
-                                id="close-btn" 
-                                data-close="true"
-                                onClick = {closeModal}
-                            >
-                                &times;
-                            </p> : ''}
-                    </div>
-                    <div className="modal-window-body" data-content>
-                        { itemContent }
-                    </div>                
-                </div>
-            </div>
-        </div>
-    )
+	const closeModal = () => hideModal()
 
+	return (
+		<div className={`vmodal ${isOpened ? 'open' : 'hide'}`}>
+			<div className="modal-overlay">
+				<div
+					className={`modal-window ${darkTheme ? 'bg-dark text-white' : ''}`}
+					style={{ top: options.positionTop }}
+				>
+					<div className="modal-window-header">
+						<h4 className="m-0">{options.title}</h4>
+						{options.closable ? (
+							<p id="close-btn" data-close="true" onClick={closeModal}>
+								&times;
+							</p>
+						) : (
+							''
+						)}
+					</div>
+					<div className="modal-window-body" data-content>
+						{itemContent}
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
